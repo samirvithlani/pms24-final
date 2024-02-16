13848a371ddc0d708ee3d868208782192d684118
samirvithlani <samir.vithlani83955@gmail.com>
2024-02-16 09:58:08 +0530

    dashboard

--- user/views.py
+++ user/views.py
@@ -1,7 +1,16 @@
+from typing import Any
+from django.forms.models import BaseModelForm
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import User
 from .forms import ManagerRegistrationForm, DeveloperRegistrationForm
+#import settings.py
+from django.conf import settings
+#send_mail is built-in function in django
+from django.core.mail import send_mail
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.views import LoginView
+from django.views.generic import ListView
 
 # Create your views here.
 class ManagerRegisterView(CreateView):
@@ -9,6 +18,16 @@ class ManagerRegisterView(CreateView):
     model = User
     form_class = ManagerRegistrationForm
     success_url = '/login/'
+    
+    def form_valid(self, form):
+        email = form.cleaned_data.get('email')
+        #print("email....",email)
+        if sendMail(email):
+            print("Mail sent successfully")
+            return super().form_valid(form)
+        else:
+            return super().form_valid(form)
+            
 
 
 class DeveloperRegisterView(CreateView):
@@ -16,3 +35,45 @@ class DeveloperRegisterView(CreateView):
     model = User
     form_class = DeveloperRegistrationForm
     success_url = '/login/'    
+
+
+
+def sendMail(to):
+    subject = 'Welcome to PMS24'
+    message = 'Hope you are enjoying your Django Tutorials'
+    #recepientList = ["samir.vithlani83955@gmail.com"]
+    recepientList = [to]
+    EMAIL_FROM = settings.EMAIL_HOST_USER
+    send_mail(subject,message, EMAIL_FROM, recepientList)
+    #attach file
+    #html
+    return True
+    
+class UserLoginView(LoginView): 
+    template_name = 'user/login.html'
+    model = User
+    
+    
+    def get_redirect_url(self):
+        if self.request.user.is_authenticated:
+            if self.request.user.is_manager:
+                return '/user/manager-dashboard/'
+            else:
+                return '/user/developer-dashboard/'
+            
+class ManagerDashboardView(ListView):
+    
+    def get(self, request, *args, **kwargs):
+        #logic to get all the projects
+        return render(request, 'user/manager_dashboard.html')
+    
+    
+    template_name = 'user/manager_dashboard.html'
+
+class DeveloperDashboardView(ListView):
+    def get(self, request, *args, **kwargs):
+        #logic to get all the projects
+        return render(request, 'user/developer_dashboard.html')
+    
+    template_name = 'user/developer_dashboard.html'    
+       
